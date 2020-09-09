a88b37046a0341f34645bf1c18f82410bfbe1f7b
Shapon Pal <shaponpal4@gmail.com>
2020-09-10 00:02:17 +0600

    dashboard UI

--- src/routes/ChatWithVisitors/index.js
+++ src/routes/ChatWithVisitors/index.js
@@ -1,28 +1,42 @@
-import { h, Component } from 'preact';
-import Card from 'preact-material-components/Card';
-import 'preact-material-components/Card/style.css';
-import 'preact-material-components/Button/style.css';
+import { h, Component } from "preact";
+import Card from "preact-material-components/Card";
+import "preact-material-components/Card/style.css";
+import "preact-material-components/Button/style.css";
+import LayoutGrid from "preact-material-components/LayoutGrid";
+import "preact-material-components/LayoutGrid/style.css";
 import List from "../../components/list";
-import style from './style';
+import Panel from "../../components/panel";
+import style from "./style";
 
 export default class ChatWithVisitors extends Component {
-	render() {
-		return (
-			<div class={`${style.home} page`}>
-				<h1>Chat With Visitors</h1>
-				<Card>
-					<div class={style.cardHeader}>
-						<h2 class=" mdc-typography--title">Dashboard card</h2>
-						<div class=" mdc-typography--caption">Welcome to home route</div>
-					</div>
-					<div class={style.cardBody}>
-						<List/>
-					</div>
-					<Card.Actions>
-						<Card.ActionButton>OKAY</Card.ActionButton>
-					</Card.Actions>
-				</Card>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div class={`${style.home} page`}>
+        <LayoutGrid>
+		<h1>Chat With Visitors</h1>
+          <LayoutGrid.Inner>
+            <LayoutGrid.Cell cols="12">
+              <Card>
+                <div class={style.cardHeader}>
+                  <h2 class=" mdc-typography--title">121 Visitors are Online </h2>
+                  <div class=" mdc-typography--caption">
+                    Show all chat Activities
+                  </div>
+                </div>
+                <div class={style.cardBody}>
+                  <List />
+                </div>
+                <Card.Actions>
+                  <Card.ActionButton>OKAY</Card.ActionButton>
+                </Card.Actions>
+              </Card>
+            </LayoutGrid.Cell>
+            <LayoutGrid.Cell cols="12">
+              <p>All Right </p>
+            </LayoutGrid.Cell>
+          </LayoutGrid.Inner>
+        </LayoutGrid>
+      </div>
+    );
+  }
 }
--- src/routes/Dashboard/index.js
+++ src/routes/Dashboard/index.js
@@ -4,8 +4,6 @@ import "preact-material-components/Card/style.css";
 import "preact-material-components/Button/style.css";
 import LayoutGrid from "preact-material-components/LayoutGrid";
 import "preact-material-components/LayoutGrid/style.css";
-import Elevation from 'preact-material-components/Elevation';
-import 'preact-material-components/Elevation/style.css';
 import List from "../../components/list";
 import Panel from "../../components/panel";
 import style from "./style";
@@ -14,33 +12,45 @@ export default class Dashboard extends Component {
   render() {
     return (
       <div class={`${style.home} page`}>
-        <h1>Dashboard</h1>
-		<LayoutGrid>
+        <LayoutGrid>
+          <h1>Dashboard</h1>
           <LayoutGrid.Inner>
-            <LayoutGrid.Cell cols="3"><Panel text="Online Visitors" value="21212"/></LayoutGrid.Cell>
-            <LayoutGrid.Cell cols="3"><Panel text="Online Visitors" value="21212"/></LayoutGrid.Cell>
-            <LayoutGrid.Cell cols="3"><Panel text="Online Visitors" value="21212"/></LayoutGrid.Cell>
-            <LayoutGrid.Cell cols="3"><Panel text="Online Visitors" value="21212"/></LayoutGrid.Cell>
-		
+            <LayoutGrid.Cell cols="3">
+              <Panel text="Online Visitors" value="21212" />
+            </LayoutGrid.Cell>
+            <LayoutGrid.Cell cols="3">
+              <Panel text="Total Client" value="3223" />
+            </LayoutGrid.Cell>
+            <LayoutGrid.Cell cols="3">
+              <Panel text="Total Operators" value="231" />
+            </LayoutGrid.Cell>
+            <LayoutGrid.Cell cols="3">
+              <Panel text="Panding Actions" value="3232" />
+            </LayoutGrid.Cell>
           </LayoutGrid.Inner>
         </LayoutGrid>
-        <Card>
-          <div class={style.cardHeader}>
-            <h2 class=" mdc-typography--title">Dashboard card</h2>
-            <div class=" mdc-typography--caption">Welcome to home route</div>
-          </div>
-          <div class={style.cardBody}>
-            <List />
-          </div>
-          <Card.Actions>
-            <Card.ActionButton>OKAY</Card.ActionButton>
-          </Card.Actions>
-        </Card>
+
         <LayoutGrid>
           <LayoutGrid.Inner>
-            <LayoutGrid.Cell cols="2"><Elevation z={4}>Z2</Elevation></LayoutGrid.Cell>
-            <LayoutGrid.Cell cols="4"><Elevation z={4}>Z2</Elevation></LayoutGrid.Cell>
-            <LayoutGrid.Cell cols="2"><Elevation z={4}>Z2</Elevation></LayoutGrid.Cell>
+            <LayoutGrid.Cell cols="12">
+              <Card>
+                <div class={style.cardHeader}>
+                  <h2 class=" mdc-typography--title">Chat Activities</h2>
+                  <div class=" mdc-typography--caption">
+                    Show all chat Activities
+                  </div>
+                </div>
+                <div class={style.cardBody}>
+                  <List />
+                </div>
+                <Card.Actions>
+                  <Card.ActionButton>OKAY</Card.ActionButton>
+                </Card.Actions>
+              </Card>
+            </LayoutGrid.Cell>
+            <LayoutGrid.Cell cols="12">
+              <p>All Right </p>
+            </LayoutGrid.Cell>
           </LayoutGrid.Inner>
         </LayoutGrid>
       </div>
